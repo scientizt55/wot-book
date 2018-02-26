1f3dab805c6b6444db0f7c085ed3c057f76ae814
scientizt55 <36810730+scientizt55@users.noreply.github.com>
2018-02-26 00:15:02 -0500

    Add files via upload

--- blink2018.js
+++ blink2018.js
@@ -5,7 +5,7 @@ var Gpio = onoff.Gpio,
 
 
 interval = setInterval(function () {
-	var value = (led.readSync() + 1) % 4;
+	var value = (led.readSync() + 1) % 20;
 	led.write(value, function() {
 		console.log("Changed LED state to: " + value);
 	});
